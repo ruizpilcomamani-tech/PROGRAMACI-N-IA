353c7745b46e32747643bb4bd1dc61b4221da1c1
ruizpilcomamani-tech <ruizpilcomamani@gmail.com>
2026-09-12 20:57:44 -0500

    Add files via upload

---
+++ NEGATIVO-POSITIVO-CERO.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h;
+    cin>>h;
+    if(h>0)
+    {
+    	cout<<"positivo";
+    	
+	}
+   else if (h<0)
+	{ 
+	cout<<"negativo";
+	
+	}
+	else
+	{ cout<<"cero";
+	}
+}
