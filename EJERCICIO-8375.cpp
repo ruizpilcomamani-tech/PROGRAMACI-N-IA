96786906290c32312977e94627e93b59b75dcba0
ruizpilcomamani-tech <ruizpilcomamani@gmail.com>
2026-09-14 10:02:26 -0500

    Add files via upload

---
+++ EJERCICIO-8375.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,h;
+	cin>>a>>b;
+	if(a>b)
+	{
+		c=a-b;
+		cout<<"el negocio va jalar "<<c;
+	}
+	else
+	{
+		h=b-a;
+		cout<<"el negocio esta muerto "<<h;
+	}
+	
+}
+
